1a2561be996b93b554f35c19e85bc75043afbe56
Casper Kaun <casper.g.kaun@gmail.com>
2023-08-30 09:09:33 +0800

    - First squash trial

--- src/main/java/com/example/githubdemo/mains.java
+++ src/main/java/com/example/githubdemo/mains.java
@@ -3,5 +3,6 @@ package com.example.githubdemo;
 public class mains {
     public static void main(String[] Args){
         System.out.println("Hello World");
+        System.out.println("For first squash");
     }
 }
