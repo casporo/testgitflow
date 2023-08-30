30b29ae990fcaa0189f82cc68f44d17327e8b07b
Casper Kaun <casper.g.kaun@gmail.com>
2023-08-30 09:14:29 +0800

    Squashed commit of the following:
    commit 40ab682aa9d24878c9798643f1d4ce641c4515e5
    Author: Casper Kaun <casper.g.kaun@gmail.com>
    Date:   Wed Aug 30 09:11:01 2023 +0800
    
        - Removed second sout
    
    commit 90dffb689fb0369cb86e0c99daaea9fc2e3eafbd
    Author: Casper Kaun <casper.g.kaun@gmail.com>
    Date:   Wed Aug 30 09:10:41 2023 +0800
    
        - Removed first sout

--- src/main/java/com/example/githubdemo/mains.java
+++ src/main/java/com/example/githubdemo/mains.java
@@ -3,8 +3,5 @@ package com.example.githubdemo;
 public class mains {
     public static void main(String[] Args){
         System.out.println("Hello World");
-        System.out.println("For first squash");
-        System.out.println("For second squash");
-
     }
 }
